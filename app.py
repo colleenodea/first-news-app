74266851730c04ad7743c5feffc302391e7e63b5
Colleen O'Dea <colleen.odea@gmail.com>
2015-03-07 10:13:46 -0500

    Added detail page

--- app.py
+++ app.py
@@ -11,12 +11,20 @@ def get_csv():
     csv_list = list(csv_obj)
     return csv_list
 
-
 @app.route("/")
 def index():
     template = "index.html"
     object_list = get_csv()
     return render_template(template, object_list=object_list)
+
+
+@app.route("/<row_id>/")
+def detail(row_id):
+    template = "detail.html"
+    object_list = get_csv()
+    for obj in object_list:
+        if obj['id'] == row_id:
+            return render_template(template, object=obj)
 
 
 if __name__ == "__main__":
